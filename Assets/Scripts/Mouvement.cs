8509d63aad5f5886fc9d1ec4470d5c419024f95e
Helbii <bilel.benrokia@outlook.fr>
2021-07-03 14:09:19 +0200

    Jump Bug

--- Assets/Scripts/Mouvement.cs
+++ Assets/Scripts/Mouvement.cs
@@ -6,21 +6,39 @@ public class Mouvement : MonoBehaviour
 {
     [SerializeField]
     private float playerspeed = 1f;
-                            
+    [SerializeField]
+    public float jumpForce = 1f;
+    private Rigidbody2D rb;
+    public BoxCollider2D col;
+    public LayerMask groundLayers; 
     // Start is called before the first frame update
     void Start()
     {
-        
-        
+        rb = GetComponent<Rigidbody2D>();
+        col = GetComponent<BoxCollider2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        float inputX = Input.GetAxis("Horizontal");
+
+        float inputX = Input.GetAxisRaw("Horizontal");
+        Vector3 movement = new Vector3(playerspeed * inputX * Time.deltaTime, 0, 0);
+        rb.AddForce(movement)
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(movement, ForceMode2D.Impulse);
+        }
         
-        Vector3 movement = new Vector3(playerspeed * inputX, 0, 0);
-        movement *= Time.deltaTime;
-        transform.Translate(movement);
+        
+
+    }
+
+
+    private void FixedUpdate()
+    {
+            
+
     }
 }
